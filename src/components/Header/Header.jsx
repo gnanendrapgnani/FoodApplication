14a49866fed06e0eaba3d018085217b798927302
Gnanendra P <gnanendrapnani@gmail.com>
2024-09-27 22:52:38 +0530

    header and navbar is created and need to complete the functionallty of the navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourites fodd here</h2>
+        <p>Choose your favourites food to order for enjoy your day</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
