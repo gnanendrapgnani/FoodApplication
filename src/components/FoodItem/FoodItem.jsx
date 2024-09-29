361a9ac6952c43e81c82521fc93bd96c165cc7fd
Gnanendra P <gnanendrapnani@gmail.com>
2024-09-29 23:16:05 +0530

    In foodItemList The Counter is implementing

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img src={image} className="food-item-image" alt="" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            {itemCount}
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-reating">
           <p>{name}</p>
-          <img src={assets.rating_starts} alt="" />
+          <img src={assets.rating_starts} />
         </div>
         <p className="food-item-description">{description}</p>
         <p className="food-item-price">${price}</p>
