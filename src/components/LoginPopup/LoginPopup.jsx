492894aa1c1be8231f9b26711c69f80170550b89
Gnanendra P <gnanendrapnani@gmail.com>
2024-10-05 07:45:37 +0530

    The LoginPopup page is completed

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -17,11 +17,30 @@ const LoginPopup = ({ setShowLogin }) => {
           />
         </div>
         <div className="login-popup-inputs">
-          <input type="text" placeholder="your name" required />
+          {currState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="your name" required />
+          )}
           <input type="email" placeholder="your name" required />
           <input type="password" placeholder="password" required />
         </div>
         <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, i agree to the terms of use & privacy policy</p>
+        </div>
+        {currState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrState("Login")}>Login here</span>
+          </p>
+        )}
       </form>
     </div>
   );
