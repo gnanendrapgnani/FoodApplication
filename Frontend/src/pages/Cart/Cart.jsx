9c86fed9526bef67dcee5ae977d26d427e5b257a
Gnanendra P <gnanendrapnani@gmail.com>
2024-10-30 08:16:11 +0530

    backend is created and floder is ready

---
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,82 @@
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
+
+const Cart = () => {
+  const { cartItem, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-Items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItem[item._id] > 0) {
+            return (
+              <div>
+                <div key={index} className="cart-Items-title cart-items-item">
+                  <img src={item.image} />
+                  <p>{item.name}</p>
+                  <p>₹{item.price}</p>
+                  <p>{cartItem[item._id]}</p>
+                  <p>{item.price * cartItem[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-detail">
+              <p>Subtotal</p>
+              <p>₹{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <p>Delivery Fee</p>
+              <p>₹{getTotalCartAmount() === 0 ? 0 : 99}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <b>Total</b>
+              <b>
+                ₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 99}
+              </b>
+            </div>
+          </div>
+          <button onClick={() => navigate("/order")}>
+            Proceed to checkout
+          </button>
+        </div>
+        <div className="cart-promocode">
+          <p>If you the promocode, Enter it here</p>
+          <div className="cart-promocode-input">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
