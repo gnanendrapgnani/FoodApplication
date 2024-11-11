005c60ba6c344d5b71dd31f30bd101a37282091a
Gnanendra P <gnanendrapnani@gmail.com>
2024-11-11 23:33:13 +0530

    need solve the error in cartData which accpet the data

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -3,14 +3,14 @@ import userModel from "../models/userModel.js";
 // add items to user Cart
 const addToCart = async (req, res) => {
   try {
-    let userData = await userModel.findOne({ _id: req.body.userID });
+    let userData = await userModel.findOne({ _id: req.body.userId });
     let cartData = await userData.cartData;
-    if (!cartData[req.body.itemID]) {
-      cartData[req.body.itemID] = 1;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
     } else {
-      cartData[req.body.itemID] += 1;
+      cartData[req.body.itemId] += 1;
     }
-    await userModel.findByIdAndUpdate(req.body.userID, { cartData });
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
     res.json({ success: true, message: "Add to cart" });
   } catch (error) {
     console.log(error);
--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = async (req, res, next) => {
-  const { token } = req.header;
+  const { token } = req.headers;
   if (!token) {
     return res.json({ success: false, message: "Not authorized login again" });
   }
   try {
     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-    req.body.userID = token_decode.id;
+    req.body.userId = token_decode.id;
     next();
   } catch (error) {
     console.log(error);
