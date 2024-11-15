0a3b38c64acd1635b12986125cfd55947c528cea
Gnanendra P <gnanendrapnani@gmail.com>
2024-11-15 08:34:09 +0530

    all the cart error as been fixed and need to connect the server to cart frontend

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModel.js";
 // add items to user Cart
 const addToCart = async (req, res) => {
   try {
-    let userData = await userModel.findOne({ _id: req.body.userId });
+    let userData = await userModel.findById(req.body.userId);
     let cartData = await userData.cartData;
     if (!cartData[req.body.itemId]) {
       cartData[req.body.itemId] = 1;
@@ -14,14 +14,36 @@ const addToCart = async (req, res) => {
     res.json({ success: true, message: "Add to cart" });
   } catch (error) {
     console.log(error);
-    res.json({ success: false, message: "Error" });
+    res.json({ success: false, message: "Errors" });
   }
 };
 
 // remove items from the user Cart
-const removeFromCart = async (req, res) => {};
+const removeFromCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Removed for Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 // fetch user cart data
-const getCart = async (req, res) => {};
+const getCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    res.json({ success: true, cartData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 export { addToCart, removeFromCart, getCart };
